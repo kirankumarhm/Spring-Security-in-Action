2d03613a2c645e8d15bbf63d9cbb3ba9d33d51c4
kirankumarhm <kirankumarhm2004@gmail.com>
2023-07-24 23:28:46 +0530

    Spring Security in Action - Second Example

---
+++ ssia-ch2-ex2/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -0,0 +1,47 @@
+package com.laurentiuspilca.ssia.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        userDetailsService.createUser(user);
+
+        return userDetailsService;
+    }
+
+    /*
+    The NoOpPasswordEncoder instance treats passwords as plain text. It doesn’t
+    encrypt or hash them. For matching, NoOpPasswordEncoder only compares
+    the strings using the underlying equals(Object o) method of the String
+    class.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
